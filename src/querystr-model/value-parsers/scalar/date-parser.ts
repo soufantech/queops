2f31fa12548448cc0ffae1c23bf57f044cd3b4e8
Fábio Priamo <fabio@soufan.com.br>
2020-08-10 22:07:24 -0300

    test: add tests for dateParser

--- src/querystr-model/value-parsers/scalar/date-parser.ts
+++ src/querystr-model/value-parsers/scalar/date-parser.ts
@@ -1,11 +1,9 @@
 import { ScalarParser, ValueParseResult } from './scalar-parser';
 
 function internalDateParser(value: string): ValueParseResult<Date> {
-  try {
-    return { value: new Date(value) };
-  } catch (_err) {
-    return { value: undefined };
-  }
+  const date = new Date(value);
+
+  return { value: isNaN(date.getTime()) ? undefined : date };
 }
 
 export function dateParser(this: void): ScalarParser<Date> {
